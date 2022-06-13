ddfcd56df1b5a562e36efef9946c073b1a3695aa
oriya <oriyaroth3@gmail.com>
2022-06-13 17:58:46 +0300

    Updates

---
+++ precache-manifest.6411b48d861b81e74a56d6fb7726fb54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a465f955e69a8585b03d",
+    "url": "/Country-site/static/css/main.dd0a1aec.chunk.css"
+  },
+  {
+    "revision": "a465f955e69a8585b03d",
+    "url": "/Country-site/static/js/main.a465f955.chunk.js"
+  },
+  {
+    "revision": "9f6f6cec55adb720d38c",
+    "url": "/Country-site/static/js/1.9f6f6cec.chunk.js"
+  },
+  {
+    "revision": "4f0c309c50bc5878bc5b",
+    "url": "/Country-site/static/js/2.4f0c309c.chunk.js"
+  },
+  {
+    "revision": "897fe972bb1add648cea",
+    "url": "/Country-site/static/js/runtime~main.897fe972.js"
+  },
+  {
+    "revision": "45b51823c2637582d4a846229c73d03d",
+    "url": "/Country-site/index.html"
+  }
+];
